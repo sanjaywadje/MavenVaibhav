d41bfbbb950512dd3db2ffdf917c786c07f4c9b4
Windows-10 <Windows-10@DESKTOP-AUHNTHL>
2022-08-07 19:00:39 +0530

    111

---
+++ src/C22/D33.java
@@ -0,0 +1,11 @@
+package C22;
+
+public class D33 {
+
+	public static void main(String[] args) {
+           System.out.println("this is LIne 1");
+           System.out.println("this is LIne 2");
+           System.out.println("this is LIne 3");
+	}
+
+}
